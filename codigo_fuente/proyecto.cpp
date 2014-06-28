9be0155585a5df72fd4baaf9c944293fe89756af
elianv <elian.vallejos@gmail.com>
2014-06-27 22:40:09 -0400

    KK

--- codigo_fuente/proyecto.cpp
+++ codigo_fuente/proyecto.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
+	char variable[100];  
 	/* code */
-	cout << "Hola mundo" << endl;
+	ifstream fp("archivo.vrp");
+	//FILE *fp;
+	//fp = fopen("archivo.vrp","r");
+	//while(fp.eof()){
+		//fscanf(fp,"%s",variable);
+		fp.getline(variable);
+		cout << "Variable leida: " << variable << endl;
+	//}
+	
 	return 0;
 }
